080b92c54f93570386fa2a4a4c9f5600ee335880
barath sam <barathsam555.p@gmail.com>
2023-10-13 15:16:26 +0530

    first commit

---
+++ build/precache-manifest.1ba7b646fded16bbeec3f4085f70753b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f1e14346356c7c71962633a5175b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b9cb252452e8506aed9",
+    "url": "/static/css/main.681f673b.chunk.css"
+  },
+  {
+    "revision": "7419bf1c83cab9abe0e9",
+    "url": "/static/js/2.36ba3cca.chunk.js"
+  },
+  {
+    "revision": "5b9cb252452e8506aed9",
+    "url": "/static/js/main.1d121932.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
